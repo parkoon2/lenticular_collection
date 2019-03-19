a45896991936f8e2fe7bccb3fe678d0dbdb14a4c
parkoon <par_koon@naver.com>
2019-03-19 09:52:53 +0900

    기본 express 서버 세팅

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import express from 'express'
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log(`Server is running on ${3000} port`);
+});
