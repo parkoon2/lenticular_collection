5e1df5419311d6b0baebaad5d023f7e238bb932c
parkoon <par_koon@naver.com>
2019-03-19 11:13:56 +0900

    1. 미들웨어 설치 2. constants 업데이트

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,23 @@
 const defaultConfig = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
 };
+
+const devConfig = {};
+
+const prodConfig = {};
+
+function envHandler(env) {
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'prod':
+      return prodConfig;
+    default:
+      return devConfig;
+  }
+}
 
 export default {
   ...defaultConfig,
+  ...envHandler(process.env.NODE_ENV),
 };
