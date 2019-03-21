00d5d71699c4bdff90b95f012dddb464a3dfe8be
parkoon <par_koon@naver.com>
2019-03-21 09:43:03 +0900

    v1.0

--- src/modules/location/location.validator.js
+++ src/modules/location/location.validator.js
@@ -14,28 +14,55 @@ export default async function validator(req, res, next) {
     QR_IMAGE_URL,
   } = req.body;
 
-  if (!USER_INFO_IDX) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'USER_INFO_IDX(number) is required' });
+  if (!USER_INFO_IDX)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'USER_INFO_IDX(number) is required' });
 
   if (!BUILDING_INFO_IDX)
-    return res.status(HttpStatus.BAD_REQUEST).json({ message: 'BUILDING_INFO_IDX(string) is required' });
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'BUILDING_INFO_IDX(string) is required' });
 
-  if (!LOCATION_CODE) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'LOCATION_CODE(string) is required' });
+  if (!LOCATION_CODE)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'LOCATION_CODE(string) is required' });
 
-  if (!FLOOR_INFO) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'FLOOR_INFO(string) is required' });
+  if (!FLOOR_INFO)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'FLOOR_INFO(string) is required' });
 
-  if (!ROOM_NUMBER) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'ROOM_NUMBER(string) is required' });
+  if (!ROOM_NUMBER)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'ROOM_NUMBER(string) is required' });
 
   if (!DETAIL_ADDRESS)
-    return res.status(HttpStatus.BAD_REQUEST).json({ message: 'DETAIL_ADDRESS(string) is required' });
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'DETAIL_ADDRESS(string) is required' });
 
-  if (!POSITION_CODE) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'POSITION_CODE(string) is required' });
+  if (!POSITION_CODE)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'POSITION_CODE(string) is required' });
 
   if (!DIRECTION_INFO)
-    return res.status(HttpStatus.BAD_REQUEST).json({ message: 'DIRECTION_INFO(string) is required' });
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'DIRECTION_INFO(string) is required' });
 
-  if (!ERROR_RADIUS) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'ERROR_RADIUS(string) is required' });
+  if (!ERROR_RADIUS)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'ERROR_RADIUS(string) is required' });
 
-  if (!QR_IMAGE_URL) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'QR_IMAGE_URL(string) is required' });
+  if (!QR_IMAGE_URL)
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ message: 'QR_IMAGE_URL(string) is required' });
 
   next();
 }
